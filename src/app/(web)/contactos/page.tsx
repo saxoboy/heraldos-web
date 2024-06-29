8ca27eb1edb91cab5c6050a2bacd0ee7beb68a2c
Israel Herrera <israel@construex.com.mx>
2024-06-29 16:09:13 -0500

    feat: Agregar componentes de etiqueta, área de texto e input
    Este commit agrega los componentes de etiqueta, área de texto e input al proyecto. Los componentes se encuentran en los archivos "label.tsx", "textarea.tsx" e "input.tsx" respectivamente. Estos componentes son utilizados para crear formularios y mejorar la experiencia del usuario al interactuar con ellos. El mensaje de commit sigue la convención establecida de usar un prefijo para indicar el tipo de cambio (feat para una nueva característica) y proporciona una descripción concisa de los cambios realizados.

--- src/app/(web)/contactos/page.tsx
+++ src/app/(web)/contactos/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+
+import { z } from "zod";
+import Link from "next/link";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Mail, MapPin, MessageCircleMore, Phone } from "lucide-react";
 import TitleHeader from "@/components/title-header";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  nombre: z.string().min(2).max(50),
+  correo: z.string().email(),
+  telefono: z.string().min(10).max(10),
+  mensaje: z.string().min(10).max(500),
+});
 
 const ContactosPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nombre: "",
+      correo: "",
+      telefono: "",
+      mensaje: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <>
       <section className="-top-[50px] relative mb-8">
@@ -13,14 +53,140 @@ const ContactosPage = () => {
             height={116}
             className="w-auto h-auto mx-auto relative z-10"
           />
-          <h1 className="hidden sm:block text-4xl md:text-5xl font-bold sm:py-4 text-center text-white">
-            <TitleHeader txtTitle="Contactos" txtSubtitle="Contactos" />
-          </h1>
         </div>
       </section>
       <section className="relative">
-        <div className="h-96">
-          <h1 className="text-7xl text-center">Seccion en Construccion </h1>
+        <div className="container flex flex-col lg:flex-row justify-start items-start gap-4 mb-12">
+          <div className="w-full lg:w-1/2">
+            <h1 className="hidden sm:block text-4xl md:text-5xl font-bold sm:py-4 text-center text-white">
+              <TitleHeader txtTitle="Contactos" txtSubtitle="Contactos" />
+            </h1>
+            <h2 className="text-3xl mb-12 ">
+              ¿Necesitas <span className="text-orange-bg">ayuda</span>?
+            </h2>
+            <p>
+              Tienes dudas, novedades o sugerencias, por favor llena el
+              siguiente formulario y nos pondremos en contacto contigo
+            </p>
+
+            <p>Para más información:</p>
+
+            <div className="flex gap-4">
+              <div className="text-orange-bg">
+                <MapPin />
+              </div>
+              <p>661 Main St, Hackensack, NJ 07601, EE. UU.</p>
+            </div>
+            <div className="flex gap-4">
+              <div className="text-orange-bg">
+                <Phone />
+              </div>
+              <p>
+                <Link href="tel:+12406448582">+1 (240) 644-8582</Link>
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <div className="text-orange-bg">
+                <Mail />
+              </div>
+              <p>
+                <Link href="mailto:info@heraldoscristo.com">
+                  info@heraldoscristo.com
+                </Link>
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <div className="text-orange-bg">
+                <MessageCircleMore />
+              </div>
+              <p>
+                <Link
+                  href="https://api.whatsapp.com/send?phone=12406448582"
+                  target="_blank"
+                >
+                  +1 (240) 644-8582
+                </Link>
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full lg:w-1/2 lg:py-32">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="nombre"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nombre y Apellido</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Ingresa tu nombre y apellido"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="correo"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Correo electrónico</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Ingresa tu correo electrónico"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="telefono"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Teléfono</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Ingresa tu número celular"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="mensaje"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Mensaje</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Escribe tu mensaje"
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" variant="outline">
+                  Enviar Mensaje
+                </Button>
+              </form>
+            </Form>
+          </div>
         </div>
       </section>
     </>
