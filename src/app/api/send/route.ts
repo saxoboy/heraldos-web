f5ddb0eb2c396465eb18d8b31a25d7f7090b5ec4
Israel Herrera <israel@construex.com.mx>
2024-08-13 20:58:02 -0500

    feat: Refactorizar ruta de envío de correo de contacto
    Este commit refactoriza la ruta de envío de correo de contacto en el archivo "route.ts". Se elimina la función "handler" y se reemplaza por una función independiente "POST" que recibe la solicitud como parámetro. Esto mejora la legibilidad y mantenibilidad del código. El mensaje de commit sigue la convención establecida de usar un prefijo para indicar el tipo de cambio (feat para una nueva característica) y proporciona una descripción concisa de los cambios realizados.

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -5,41 +5,66 @@ import { NextApiRequest, NextApiResponse } from "next";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const emailContact = process.env.EMAIL_CONTACT;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log(`Method: ${req.method}`);
-  if (req.method === "POST") {
-    const { nombre, correo, telefono, mensaje } = req.body;
+export async function POST(req: any) {
+  const { nombre, correo, telefono, mensaje } = req.body;
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["israelherrerae@gmail.com"], // Reemplaza con la dirección de correo de destino
+      subject: `Nuevo mensaje de contacto de ${nombre}`,
+      react: EmailTemplate({
+        nombre,
+        correo,
+        telefono,
+        mensaje,
+      }) as React.ReactElement, // Puedes adaptar la plantilla para incluir otros datos
+    });
 
-    try {
-      if (!emailContact) {
-        return res.status(500).json({ error: "Email contact not defined" });
-      }
-
-      const { data, error } = await resend.emails.send({
-        from: "Acme <onboarding@resend.dev>",
-        to: ["israelherrerae@gmail.com"], // Reemplaza con la dirección de correo de destino
-        subject: `Nuevo mensaje de contacto de ${nombre}`,
-        react: EmailTemplate({
-          nombre,
-          correo,
-          telefono,
-          mensaje,
-        }) as React.ReactElement, // Puedes adaptar la plantilla para incluir otros datos
-      });
-
-      if (error) {
-        return res.status(500).json({ error });
-      }
-
-      return res.status(200).json(data);
-    } catch (error) {
-      return res.status(500).json({ error });
+    if (error) {
+      return Response.json({ error }, { status: 500 });
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
   }
 }
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   console.log(`Method: ${req.method}`);
+//   if (req.method === "POST") {
+//     const { nombre, correo, telefono, mensaje } = req.body;
+
+//     try {
+//       if (!emailContact) {
+//         return res.status(500).json({ error: "Email contact not defined" });
+//       }
+
+//       const { data, error } = await resend.emails.send({
+//         from: "Acme <onboarding@resend.dev>",
+//         to: ["israelherrerae@gmail.com"], // Reemplaza con la dirección de correo de destino
+//         subject: `Nuevo mensaje de contacto de ${nombre}`,
+//         react: EmailTemplate({
+//           nombre,
+//           correo,
+//           telefono,
+//           mensaje,
+//         }) as React.ReactElement, // Puedes adaptar la plantilla para incluir otros datos
+//       });
+
+//       if (error) {
+//         return res.status(500).json({ error });
+//       }
+
+//       return res.status(200).json(data);
+//     } catch (error) {
+//       return res.status(500).json({ error });
+//     }
+//   } else {
+//     res.setHeader("Allow", ["POST"]);
+//     return res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
