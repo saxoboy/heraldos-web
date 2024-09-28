096f9f4bae12b32aa2c8da748460f329949f5325
Israel Herrera <israel@construex.com.mx>
2024-09-28 11:35:04 -0500

    Refactorizar manejo de selección de tallas y cantidades en el formulario de tienda

--- src/components/form-tienda.tsx
+++ src/components/form-tienda.tsx
@@ -26,7 +26,8 @@ import { ETalla, IProduct, products } from "@/data/productos";
 import "react-international-phone/style.css";
 
 interface ISelectedProduct extends IProduct {
-  talla?: string; // Añade la talla seleccionada opcionalmente
+  cantidad: number;
+  tallas?: string[]; // Array de tallas seleccionadas, uno por cada cantidad
 }
 
 export default function FormTienda() {
@@ -36,7 +37,7 @@ export default function FormTienda() {
     email: "",
     phone: "",
     county: "",
-    selectedItems: [] as ISelectedProduct[], // Ahora guarda los productos seleccionados
+    selectedItems: [] as ISelectedProduct[],
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -44,14 +45,12 @@ export default function FormTienda() {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  // Maneja la selección del producto (checkbox)
   const handleProductSelect = (product: IProduct) => {
     setFormData((prev) => {
       const isAlreadySelected = prev.selectedItems.some(
         (item) => item.id === product.id
       );
       if (isAlreadySelected) {
-        // Si ya está seleccionado, lo quitamos
         return {
           ...prev,
           selectedItems: prev.selectedItems.filter(
@@ -59,57 +58,63 @@ export default function FormTienda() {
           ),
         };
       } else {
-        // Si no está seleccionado, lo agregamos
         return {
           ...prev,
-          selectedItems: [...prev.selectedItems, product],
+          selectedItems: [...prev.selectedItems, { ...product, cantidad: 1 }],
         };
       }
     });
   };
 
-  // Maneja la selección de talla para un producto
-  const handleSizeSelect = (productId: number, size: string) => {
+  const handleQuantityChange = (productId: number, quantity: number) => {
     setFormData((prev) => ({
       ...prev,
       selectedItems: prev.selectedItems.map((item) =>
-        item.id === productId ? { ...item, talla: size } : item
+        item.id === productId ? { ...item, cantidad: quantity } : item
+      ),
+    }));
+  };
+
+  const handleSizeSelect = (productId: number, index: number, size: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      selectedItems: prev.selectedItems.map((item) =>
+        item.id === productId
+          ? {
+              ...item,
+              tallas: item.tallas
+                ? Object.assign([], item.tallas, { [index]: size })
+                : Array(item.cantidad).fill(""),
+            }
+          : item
       ),
     }));
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    console.log(formData);
+    // try {
+    //   const response = await fetch("/api/sendEmail", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formData),
+    //   });
 
-    try {
-      const response = await fetch("/api/sendEmail", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+    //   const result = await response.json();
 
-      const result = await response.json();
-
-      if (response.ok) {
-        console.log("Correo enviado:", result.message);
-        // Cierra el modal si es necesario
-        setIsOpen(false);
-      } else {
-        console.error("Error al enviar el correo:", result.message);
-      }
-    } catch (error) {
-      console.error("Error al enviar el correo:", error);
-    }
+    //   if (response.ok) {
+    //     console.log("Correo enviado:", result.message);
+    //     setIsOpen(false);
+    //   } else {
+    //     console.error("Error al enviar el correo:", result.message);
+    //   }
+    // } catch (error) {
+    //   console.error("Error al enviar el correo:", error);
+    // }
   };
-
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   console.log("Order placed:", formData);
-  //   setIsOpen(false);
-  //   // Aquí puedes hacer el submit del formulario con los productos y sus tallas
-  // };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -149,7 +154,7 @@ export default function FormTienda() {
             <Label htmlFor="phone">Teléfono</Label>
             <PhoneInput
               name="phone"
-              inputClassName="w-full focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              inputClassName="w-full"
               defaultCountry="us"
               value={formData.phone}
               onChange={(phone: string) =>
@@ -170,7 +175,7 @@ export default function FormTienda() {
           {/* Listado de productos */}
           <div className="lg:col-span-2 space-y-4">
             <Label>Seleccione artículos</Label>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
               {products.map((product) => (
                 <div key={product.id} className="space-y-2 border p-4">
                   <div className="flex items-center">
@@ -191,41 +196,78 @@ export default function FormTienda() {
                     alt={product.name}
                     width={150}
                     height={150}
-                    style={{
-                      aspectRatio: "200/200",
-                      objectFit: "cover",
+                    style={{ 
+                      objectFit: "cover", 
+                      aspectRatio: "1/1"
                     }}
                   />
 
-                  {/* Si el producto tiene tallas, mostrar el selector */}
+                  {/* Selector de cantidad */}
+                  {formData.selectedItems.some(
+                    (item) => item.id === product.id
+                  ) && (
+                    <div className="space-y-2">
+                      <Label htmlFor={`cantidad-${product.id}`}>Cantidad</Label>
+                      <Input
+                        id={`cantidad-${product.id}`}
+                        type="number"
+                        min="1"
+                        value={
+                          formData.selectedItems.find(
+                            (item) => item.id === product.id
+                          )?.cantidad || 1
+                        }
+                        onChange={(e) =>
+                          handleQuantityChange(
+                            product.id,
+                            Number(e.target.value)
+                          )
+                        }
+                      />
+                    </div>
+                  )}
+
+                  {/* Si el producto tiene tallas, mostrar el selector de talla por cada cantidad */}
                   {product.talla &&
                     formData.selectedItems.some(
                       (item) => item.id === product.id
                     ) && (
-                      <Select
-                        value={
-                          formData.selectedItems.find(
-                            (item) => item.id === product.id
-                          )?.talla || ""
-                        }
-                        onValueChange={(value) =>
-                          handleSizeSelect(product.id, value)
-                        }
-                      >
-                        <SelectTrigger className="w-[180px]">
-                          <SelectValue placeholder="Seleccionar Talla" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectLabel>Talla</SelectLabel>
-                            <SelectItem value={ETalla.S}>S</SelectItem>
-                            <SelectItem value={ETalla.M}>M</SelectItem>
-                            <SelectItem value={ETalla.L}>L</SelectItem>
-                            <SelectItem value={ETalla.XL}>XL</SelectItem>
-                            <SelectItem value={ETalla.XXL}>XXL</SelectItem>
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
+                      <>
+                        <Label>Tallas por cantidad</Label>
+                        {[
+                          ...Array(
+                            formData.selectedItems.find(
+                              (item) => item.id === product.id
+                            )?.cantidad || 1
+                          ),
+                        ].map((_, index) => (
+                          <Select
+                            key={index}
+                            value={
+                              formData.selectedItems.find(
+                                (item) => item.id === product.id
+                              )?.tallas?.[index] || ""
+                            }
+                            onValueChange={(value) =>
+                              handleSizeSelect(product.id, index, value)
+                            }
+                          >
+                            <SelectTrigger className="w-[180px]">
+                              <SelectValue placeholder="Seleccionar Talla" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectLabel>Talla</SelectLabel>
+                                <SelectItem value={ETalla.S}>S</SelectItem>
+                                <SelectItem value={ETalla.M}>M</SelectItem>
+                                <SelectItem value={ETalla.L}>L</SelectItem>
+                                <SelectItem value={ETalla.XL}>XL</SelectItem>
+                                <SelectItem value={ETalla.XXL}>XXL</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        ))}
+                      </>
                     )}
                 </div>
               ))}
